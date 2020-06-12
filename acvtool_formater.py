369a2b2a0cc66bbd49d282384c20f58becbf2577
migueltouret <miguel_touret@hotmail.com>
2020-06-12 15:17:49 +0100

    pep8 formatting

--- acvtool_formater.py
+++ acvtool_formater.py
@@ -4,10 +4,10 @@ import re
 
 with open("index.html", 'r') as file:
     soup = BeautifulSoup(file, 'html.parser')
-    package_name = soup.body.ul.li.a.contents[0].replace(".","\.")
+    package_name = soup.body.ul.li.a.contents[0].replace(".", "\.")
     print(package_name)
-    values = soup.select("tr:has(> td:has(> a:contains(" + package_name + ")))")
-
+    values = soup.select(
+        "tr:has(> td:has(> a:contains(" + package_name + ")))")
     soup.body.table.tbody.contents = values
 
     table = soup.body.tbody
@@ -20,8 +20,8 @@ with open("index.html", 'r') as file:
     classes_missed_sum = 0
     classes_total_sum = 0
 
-
-    for i in table_values:
+    for i in values:
+        # print(i)
         actual_values = i.find_all("td")
         lines_missed = actual_values[3].contents[0]
         lines_total = actual_values[4].contents[0]
@@ -37,17 +37,10 @@ with open("index.html", 'r') as file:
         classes_missed_sum += int(classes_missed)
         classes_total_sum += int(classes_total)
 
-    print(lines_missed_sum)
-    print(lines_total_sum)
-    # print(methods_missed_sum)
-    # print(methods_total_sum)
-    # print(classes_missed_sum)
-    # print(classes_total_sum)
-
     total = soup.body.table.tfoot.tr.find_all("td")
 
     lines_covered = lines_total_sum - lines_missed_sum
-    coverage_percentage = round(lines_covered / lines_total_sum  * 100, 5)
+    coverage_percentage = round(lines_covered / lines_total_sum * 100, 5)
     total[1].string = str(lines_covered) + " of " + str(lines_total_sum)
     total[2].string = str(coverage_percentage) + "%"
     total[3].string = str(lines_missed_sum)
@@ -57,34 +50,6 @@ with open("index.html", 'r') as file:
     total[7].string = str(classes_missed_sum)
     total[8].string = str(classes_total_sum)
 
-
     newfile = open("new.html", "w")
     newfile.write(str(soup))
     newfile.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # table = soup.body.tbody
-    # table_children = table.contents
-    # print(table)
-
-
-        # for i in table:
-    #     print(i.find_all("a" , text=re.compile('^' + package_name)))
-    # aaa = [x.parent.parent for x in package_columns]
-    # for tag in package_columns:
-    #     tag.decompose()
-        #.decompose()
